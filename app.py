2245b7f16c4b9a2c48e9709fbcd59ce029f17c4e
SebasA0306 <jsebastianam.120@gmail.com>
2026-05-15 07:41:56 -0500

    Add dynamic Flask endpoints and filtering

--- app.py
+++ app.py
@@ -9,6 +9,28 @@ df = pd.read_csv("data/processed/cleaned_electricity_data.csv")
 def home():
     return{ "message": "Electricity Consumption API" }
 
+#Endpoint dinámico
+@app.route("/device/<device_name>")
+def get_device_data(device_name):
+    filtered_data=df[df["device"].str.lower()==device_name.lower()]
+    
+    if filtered_data.empty:
+        return{"error":"Device not found"},404
+    
+    return filtered_data.head(10).to_dict(orient="records")
+
+@app.route("/top-devices")
+def top_devices():
+    ranking = df.groupby("device")["consumption_kwh"].sum()
+    ranking = ranking.sort_values(ascending=False)
+    return ranking.head(5).to_dict()
+
+@app.route("/peak-hour")
+def peak_hour():
+    hourly_consumption =df.groupby("hour")["consumption_kwh"].mean()
+    peak=hourly_consumption.idxmax()
+    return {"peak_hour":int(peak)}
+
 @app.route("/devices")
 def get_devices():
     
